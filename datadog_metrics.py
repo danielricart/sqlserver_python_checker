dd13bb10d93b77d4518a9757feb6984b5fc5352e
Daniel Ricart <daniel.ricart@scmspain.com>
2017-11-13 17:44:18 +0100

    Added datadog module

---
+++ datadog_metrics.py
@@ -0,0 +1,33 @@
+from datadog import initialize, api
+import time
+
+
+def submit_custom_metric(metric_name, value, api_key, app_key, timestamp=int(time.time())):
+    options = {
+        'api_key': api_key,
+        'app_key': app_key
+    }
+
+    data_point = (timestamp, value)
+    initialize(**options)
+
+    now = time.time()
+    try:
+        # Submit a point with a timestamp (must be ~current)
+        response = api.Metric.send(metric=metric_name, points=data_point)
+        if response['status'] == "ok":
+            print('Failed to submit custom metric.')
+        else:
+            print("Published.")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    submit_custom_metric(
+        metric_name="middleware.tests.metrics",
+        value=66,
+        api_key="ca3d835abbbef6afba3ec20d890f65a9",
+        app_key="627daeae44f1982d1e64a2e79ac7d1f9c9d33419"
+    )
+# --datadog_apikey=ca3d835abbbef6afba3ec20d890f65a9 --datadog_appkey=627daeae44f1982d1e64a2e79ac7d1f9c9d33419
