870e511fc6a2376725641ee22c91016fda3a2df6
Daniel Ricart <daniel.ricart@scmspain.com>
2018-02-19 15:21:43 +0100

    module commit

---
+++ query_builder/QueryBuilder.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+import random
+from pprint import pprint
+
+
+class QueryBuilder:
+    def __init__(self):
+        return
+
+    @staticmethod
+    def check(query):
+        results = []
+
+        if isinstance(query, list):
+            for item in query:
+                results.extend(QueryBuilder.check(item))
+        elif not isinstance(query, dict):
+            raise TypeError("expected dict{{namespace, query, [parameters]}}. Got {}".format(type(query)))
+        elif isinstance(query, dict) and len(query) > 0:
+            if "parameters" in query:
+                for param_name, parameter in query["parameters"].items():
+                    current_query = query["query"].format(parameter)
+                    current_namespace = ".".join([query["namespace"], param_name])
+                    # print("{0} {1}".format(current_namespace, current_query))
+                    results.append({"namespace": current_namespace, "query": current_query})
+            else:
+                results.append({"namespace": query["namespace"], "query": query["query"]})
+        return results
